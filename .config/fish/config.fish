e26a66ae55e5efdcf87890d9d46e20e8c6bc385a
hucancode <hucancode@gmail.com>
2022-04-25 11:10:10 +0900

    fish macos compatibility config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
+set script_dir (dirname (status --current-filename))
 if status is-interactive
-    set fish_color_valid_path
     # Commands to run in interactive sessions can go here
+    set fish_color_valid_path
     set -x EDITOR helix
     set -x FZF_DEFAULT_COMMAND "fd --exclude={.git,.idea,.vscode,node_modules,build} --type f"
     set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
+    if test -f $script_dir/local.fish
+        source $script_dir/local.fish
+    end
 end
