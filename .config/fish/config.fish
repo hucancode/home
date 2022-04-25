a7ff7a26ed03ffdd7abc64a0ac3f15d3dabb70f4
hucancode <hucancode@gmail.com>
2022-04-25 11:55:18 +0900

    fix macos SHELL env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_color_valid_path
     set fish_greeting
+    set -x SHELL fish
     set -x EDITOR helix
     set -x FZF_DEFAULT_COMMAND "fd --exclude={.git,.idea,.vscode,node_modules,build} --type f"
     set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
