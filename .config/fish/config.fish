734fdb5d05dc9e205b8baecf90e8268df7906896
hucancode <hucancode@gmail.com>
2022-04-27 23:32:48 +0900

    fix fish config, always source local source

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_color_valid_path
     set fish_greeting
-    set -x SHELL fish
-    set -x EDITOR helix
-    set -x FZF_DEFAULT_COMMAND "fd --exclude={.git,.idea,.vscode,node_modules,build} --type f"
-    set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
-    if test -e $script_dir/local.fish
-        source $script_dir/local.fish
-    end
+    set -gx SHELL fish
+    set -gx EDITOR helix
+    set -gx FZF_DEFAULT_COMMAND "fd --exclude={.git,.idea,.vscode,node_modules,build} --type f"
+    set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500'"
+end
+if test -e $script_dir/local.fish
+    source $script_dir/local.fish
 end
