5d7bf25598c38eebd73fb8dbccd12b687157f7ce
hucancode <hucancode@gmail.com>
2022-05-14 15:09:14 +0900

    cleaner lua config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,14 @@
 require('plugins')
-vim.cmd [[set mouse+=a]]
-vim.cmd [[set clipboard=unnamedplus]]
-vim.cmd [[colorscheme catppuccin]]
-vim.cmd [[set relativenumber number]]
-vim.cmd [[set expandtab smartindent]]
-vim.cmd [[set tabstop=2]]
-vim.cmd [[set shiftwidth=2]]
-vim.cmd [[let g:netrw_banner=0]]
+vim.cmd("colorscheme catppuccin")
+table.insert(vim.opt.mouse, "a")
+vim.opt.clipboard = "unnamedplus"
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.g.netrw_banner = 0
 vim.api.nvim_set_keymap("n", "<space><tab>", ":bnext<cr>", {noremap = true})
 vim.api.nvim_set_keymap("", "d", "\"_d", {noremap = true})
 vim.api.nvim_set_keymap("", "D", "\"_D", {noremap = true})
