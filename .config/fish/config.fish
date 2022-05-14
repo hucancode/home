09fbf58b8987560224ce267daefb0e3ded392fdd
hucancode <hucancode@gmail.com>
2022-05-14 21:16:35 +0900

    update fish default editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
     set fish_color_valid_path
     set fish_greeting
     set -gx SHELL fish
-    set -gx EDITOR helix
+    set -gx EDITOR nvim
     set -gx FZF_DEFAULT_COMMAND "fd --exclude={.git,.idea,.vscode,node_modules,build} --type f"
     set -gx FZF_DEFAULT_OPTS "--height 40% --layout=reverse 2> /dev/null | head -500'"
     starship init fish | source
