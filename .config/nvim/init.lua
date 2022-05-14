2b7288fd96dbb53c785b53bfb2f71eb0cc11a39b
hucancode <hucancode@gmail.com>
2022-05-14 14:30:39 +0900

    cleaner netrw

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,7 @@ vim.cmd [[set relativenumber number]]
 vim.cmd [[set expandtab smartindent]]
 vim.cmd [[set tabstop=2]]
 vim.cmd [[set shiftwidth=2]]
+vim.cmd [[let g:netrw_banner=0]]
 vim.api.nvim_set_keymap("n", "<space><tab>", ":bnext<cr>", {noremap = true})
 vim.api.nvim_set_keymap("", "d", "\"_d", {noremap = true})
 vim.api.nvim_set_keymap("", "D", "\"_D", {noremap = true})
