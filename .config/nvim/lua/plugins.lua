54a874d88874e7e1bc8824da5620be270b8c2b74
hucancode <hucancode@gmail.com>
2022-05-23 09:55:16 +0900

    fix typescript lsp server name typo

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -139,7 +139,7 @@ function lsp(use)
     })
     local lsp = require('lspconfig')
     local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-    local servers = {'clangd', 'tailwindcss', 'svelte', 'html', 'cssls', 'typescript'}
+    local servers = {'clangd', 'tailwindcss', 'svelte', 'html', 'cssls', 'tsserver'}
     for i = 1, #servers do
         lsp[servers[i]].setup { capabilities = capabilities }
     end
