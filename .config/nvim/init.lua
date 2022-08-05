102b437e0c46e6122905d3915a53f06e04827bbf
Bang Nguyen Huu <hucancode@gmail.com>
2022-08-05 15:48:39 +0900

    neovim, add wsl copy paste support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,6 +12,22 @@ if not vim.g.vscode then
   vim.opt.tabstop = 2
   vim.opt.shiftwidth = 2
 end
+
+if vim.fn.executable('win32yank') == 1 then 
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank -i --crlf",
+      ["*"] = "win32yank -i --crlf"
+    },
+    paste = {
+      ["+"] = "win32yank -o --lf",
+      ["*"] = "win32yank -o --lf"
+    },
+    cache_enable = 0,
+  }
+end
+
 vim.opt.clipboard = "unnamedplus"
 vim.api.nvim_set_keymap("", "d", "\"_d", {noremap = true})
 vim.api.nvim_set_keymap("", "D", "\"_D", {noremap = true})
