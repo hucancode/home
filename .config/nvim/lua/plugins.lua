c46995ea3a5a93122917e4f7b62a307a8e349a08
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-13 14:19:19 +0900

    use "let g:CPTimeLimit=2" to set CP time limit to 2s

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -209,8 +209,9 @@ function lsp()
 end
 
 function cp()
+    vim.cmd 'let g:CPTimeLimit=2'
     vim.cmd 'command NewCP :read ~/.config/cp/template.%:e <bar> :e %:r.in <bar> :b#'
-    vim.cmd 'command BuildCP !g++ -std=c++17 % -o %:r && ./%:r <%:r.in >%:r.out'
+    vim.cmd "command BuildCP :execute '!g++ -std=c++17 % -o %:r && timeout 'CPTimeLimit' ./%:r <%:r.in >%:r.out'"
     vim.cmd 'autocmd filetype cpp nnoremap <F5> :w <bar> :BuildCP <CR>'
 end
 function setup(use)
