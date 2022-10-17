cd35844d1dbe200026db005dae14f2f5ff6b34a0
Bang Nguyen Huu <hucancode@gmail.com>
2022-10-18 08:53:52 +0900

    Update template.cpp

--- .config/cp/template.cpp
+++ .config/cp/template.cpp
@@ -1,5 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define FOR(i,a,b) for (int i = a; i <= b; i++)
+#define RFOR(i,a,b) for (int i = a; i >= b; i--)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 
 void solve() {
 }
