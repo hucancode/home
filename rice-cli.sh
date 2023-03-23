67a7a378334486b1439663120c8f1bccbb32f612
hucancode <hucancode@gmail.com>
2023-03-24 07:42:19 +0900

    clean up script

---
+++ rice-cli.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/sh
+sudo pacman -Syu base-devel git
+# change the following line to your username
+GH_USERNAME=hucancode
+# change the following line to your github repository
+GH_REPO=home
+# checkout configurations
+git init
+git remote add origin https://github.com/$GH_USERNAME/$GH_REPO
+git fetch
+git reset origin/main
+git checkout -t origin/main
+git submodule update --init --recursive
+# install yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin && makepkg -si && cd -
+# install packages
+FILE_MANAGER_AUR="lf"
+TERMINAL="neovim fzf fd ripgrep exa git-delta"
+yay -S $TERMINAL $FILE_MANAGER_AUR
+# neovim first time setting
+# nvim -c "PackerInstall"
--- rice.sh
+++ rice.sh
@@ -1,12 +1,9 @@
-#! /usr/bin/env fish
-# run the following line in bash before running this script
-# sudo pacman -Syu fish
-chsh -s $(which fish)
-sudo pacman -S base-devel git
+#! /usr/bin/sh
+sudo pacman -Syu base-devel git
 # change the following line to your username
-set GH_USERNAME hucancode
+GH_USERNAME=hucancode
 # change the following line to your github repository
-set GH_REPO home
+GH_REPO=home
 # checkout configurations
 git init
 git remote add origin https://github.com/$GH_USERNAME/$GH_REPO
@@ -16,42 +13,22 @@ git checkout -t origin/main
 git submodule update --init --recursive
 # install yay
 git clone https://aur.archlinux.org/yay-bin.git
-begin cd yay-bin; and makepkg -si; end
+cd yay-bin && makepkg -si && cd -
 # install packages
-set THEME "lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings arc-gtk-theme"
-set FONTS "ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji"
-set WM "openbox rofi yad dunst xdg-user-dirs nitrogen xclip xdotool xorg-xsetroot maim"
-set WM_AUR "i3lock-color eww-git ksuperkey"
-set MEDIA "alsa-utils ffmpeg mpd mpc mpv viewnior mousepad"
-set MEDIA_AUR "vieb-bin"
-set FILE_MANAGER_AUR "lf"
-set TERMINAL "kitty neovim fzf fd ripgrep exa git-delta"
-set KEYRING "gnome-keyring libgnome-keyring"
-sudo pacman -S $THEME $FONTS $WM $MEDIA $TERMINAL $KEYRING
-yay -S $WM_AUR $MEDIA_AUR $FILE_MANAGER_AUR
+THEME="lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings arc-gtk-theme"
+FONTS="ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji"
+WM="openbox rofi yad dunst xdg-user-dirs nitrogen xclip xdotool xorg-xsetroot maim"
+WM_AUR="i3lock-color eww-git ksuperkey"
+MEDIA="alsa-utils ffmpeg mpd mpc mpv viewnior mousepad"
+MEDIA_AUR="vieb-bin"
+FILE_MANAGER_AUR="lf"
+TERMINAL="kitty neovim fzf fd ripgrep exa git-delta"
+KEYRING="gnome-keyring libgnome-keyring"
+yay -S $THEME $FONTS $WM $MEDIA $TERMINAL $KEYRING $WM_AUR $MEDIA_AUR $FILE_MANAGER_AUR
 # neovim first time setting
 # nvim -c "PackerInstall"
 # wallpaper/icon settings
-curl -L -o avatar.png https://github.com/$GH_USERNAME.png; and sudo cp {avatar.png,.config/lightdm/wallpaper.jpg} /usr/share/lightdm-gtk-greeter-settings; and rm avatar.png
-sudo mkdir /usr/share/openbox; and sudo mv ~/.config/openbox/icons /usr/share/openbox
+curl -L -o avatar.png https://github.com/$GH_USERNAME.png && sudo cp {avatar.png,.config/lightdm/wallpaper.jpg} /usr/share/lightdm-gtk-greeter-settings && rm avatar.png
+sudo mkdir /usr/share/openbox && sudo mv .config/openbox/icons /usr/share/openbox
 sudo systemctl enable lightdm
-sudo mkdir -p /etc/lightdm; and sudo cp .config/lightdm/lightdm-gtk-greeter.conf /etc/lightdm
-# bloat sections
-read -p "Do you wish to add more bloat to you system? [y/N] " yn
-switch $yn
-	case y Y
-	case '*'
-		exit 0
-end
-# install bloats
-set PERSONAL_BLOAT "ibus-anthy"
-set PERSONAL_BLOAT_AUR "ibus-bamboo megacmd teamviewer"
-set DEV_BLOAT "docker docker-compose mono krita clang llvm"
-sudo pacman -S $PERSONAL_BLOAT $DEV_BLOAT
-yay -S $PERSONAL_BLOAT_AUR
-# post-install setup
-sudo groupadd docker; and sudo usermod -aG docker $USER
-set IBUS_SETTING "\n# Settings for Japanese input\nexport GTK_IM_MODULE=QT_IM_MODULE=XMODIFIERS=@im='ibus'\n# Toolbar for ibus\nibus-daemon -drx"
-echo -e ${IBUS_SETTING} >> ~/.xprofile
-# steam require multilib, uncomment the [multilib] section in /etc/pacman.conf
-sudo pacman -S steam
+sudo mkdir -p /etc/lightdm && sudo cp .config/lightdm/lightdm-gtk-greeter.conf /etc/lightdm
