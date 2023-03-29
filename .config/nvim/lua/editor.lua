9c1faa40ab1c633c5fc9f1043ef4ef417cff7528
hucancode <hucancode@gmail.com>
2023-03-29 14:43:29 +0900

    nvim now has transparent bg

--- .config/nvim/lua/editor.lua
+++ .config/nvim/lua/editor.lua
@@ -11,3 +11,6 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.api.nvim_set_hl(0, "Normal", { guibg = none })
+vim.api.nvim_set_hl(0, "NormalNC", { guibg = none })
+vim.api.nvim_set_hl(0, "NormalFloat", { guibg = none })
