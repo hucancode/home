5f99f86de8a68cb3ade81e6463a7cf90e209d6c4
hucancode <hucancode@gmail.com>
2023-11-10 23:18:31 +0900

    git status in fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -4,5 +4,13 @@ function fish_prompt
     if test $last_exit_status -eq 0
         set character_color normal
     end
-    echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (set_color brblue) (fish_vcs_prompt) (set_color $character_color) ' ᐉ '
+    set -g __fish_git_prompt_showcolorhints true
+    set -g __fish_git_prompt_showstashstate true
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_char_stashstate '☰'
+    set -g __fish_git_prompt_char_stagedstate '~'
+    set -g __fish_git_prompt_char_dirtystate '*'
+    set -g __fish_git_prompt_char_untrackedfiles '≈'
+    set -g __fish_git_prompt_char_invalidstate '×'
+    echo -n -s (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (fish_vcs_prompt) (set_color $character_color) ' ᐉ '
 end
